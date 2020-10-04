351f3511cb6a73bee9612ac2a201f4c7335014aa
mahinyayaa <mahinyayaa@gmail.com>
2020-10-04 11:45:45 -0700

    half-done

--- index.js
+++ index.js
@@ -1,20 +1,92 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
+const admin = require('firebase-admin');
+const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lmoae.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+require('dotenv').config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lmoae.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors())
+app.use(bodyParser.json());
+
+const serviceAccount = require("./volunteer-network00-firebase-adminsdk-pe49g-f0f997f65e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://volunteer-network00.firebaseio.com"
+});
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
 
-  // perform actions on the collection object
+client.connect(err => {
+  const activitiesCollection = client.db("volunteerNetwork").collection("activities");
+  const registerCollection = client.db("volunteerNetwork").collection("userData");
+
+  app.post('/addActivities', (req, res) => {
+    const volunteerActivities = req.body;
+    activitiesCollection.insertMany(volunteerActivities)
+    .then(result => {
+      console.log(result)
+      res.send(result)
+    })
+  })
+
+  app.get('/activities', (req, res) => {
+    activitiesCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+  app.post('/register', (req, res) => {
+    const registerData = req.body;
+    registerCollection.insertOne(registerData)
+    .then(result => {
+      console.log(result)
+      res.send(result)
+    })
+  })
+
+  app.get('/allRegister', (req, res) => {
+    const bearer = req.headers.authorization;
+
+    if(bearer && bearer.startsWith('Bearer ')){
+      const idToken = bearer.split(' ')[1];
+
+      admin.auth().verifyIdToken(idToken)
+        .then(function(decodedToken) {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+
+          if(tokenEmail == queryEmail){
+            registerCollection.find({email: queryEmail})
+              .toArray((err, documents) => {
+                res.status(200).send(documents)
+              })
+          }
+          else{
+            res.status(401).send('un-authorized access')
+          }
+        })
+        .catch(function(error) {
+          res.status(401).send('un-authorized access')
+        });
+    } 
+    else{
+      res.status(401).send('un-authorized access')
+    }
+  })
+
+  app.delete('/deleteActivity/:id', (req, res) => {
+    registerCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then(result => {
+      console.log(result)
+    })
+  })
+
   app.get('/', (req, res) => {
     res.send('Hello World!')
   })
